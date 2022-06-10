b616051f44d6a7a2e0a938423748102f3f84763c
thayssa-souza <thayssa.souzaf@gmail.com>
2022-06-10 20:42:47 -0300

    Adicionar arquivos de projeto.

---
+++ letscode-Aula05/Caixa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letscode_Aula05
+{
+    class caixaDoPetshop
+    {
+        public static void Main(string[] args)
+        {
+        Caixa caixaDoDia = new Caixa();
+        caixaDoDia.AbrirCaixa(); //entao primeiro abrimos o caixa no mercado
+
+        Console.WriteLine($"Caixa aberto em: {caixaDoDia.DataCaixa} ");
+        Console.WriteLine($"Saldo do caixa: R${caixaDoDia.Saldo}");
+
+        caixaDoDia.Deposita(10M);
+        Console.WriteLine($"Saldo do caixa: R${caixaDoDia.Saldo}");
+
+        caixaDoDia.Deposita(50M);
+        Console.WriteLine($"Saldo do caixa: R${caixaDoDia.Saldo}");
+
+        caixaDoDia.Saque(20);
+        Console.WriteLine($"Saldo do caixa: R${caixaDoDia.Saldo}");
+
+        caixaDoDia.Saque(70M);
+        Console.WriteLine($"Saldo do caixa: R${caixaDoDia.Saldo}");
+
+        caixaDoDia.CalcularTroco();
+        Console.WriteLine(caixaDoDia.CalcularTroco);
+        Console.WriteLine("-----------------------------");
+        caixaDoDia.DevolverTroco();
+        Console.WriteLine($"Saldo do caixa: R${caixaDoDia.DevolverTroco}");
+    }
+}
+}
---
+++ letscode-Aula05/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letscode_Aula05
+{
+        public class Caixa
+        {
+            public DateTime DataCaixa { get; private set; }
+            public decimal Saldo { get; private set; }
+            public void AbrirCaixa(decimal saldoInicial = 0) //o decimal valor indica o caixa inicial
+            {
+                DataCaixa = DateTime.Now;
+                Saldo = saldoInicial;
+            }
+            public void Deposita(decimal valor)
+            {
+                Saldo += valor; //soma com o valor já existente
+            }
+
+            public void Saque(decimal valor)
+            {
+                if (Saldo >= valor)
+                    Saldo -= valor;
+                else
+                {
+                    Console.WriteLine("Saldo insuficiente");
+                    Console.WriteLine($"O saldo atual é R${Saldo}");
+                }
+                //CalcularTroco - passando o valor da venda e o valor dado pelo cliente
+            }
+            public decimal CalcularTroco()
+            {
+                decimal troco = 0M;
+                Console.WriteLine("Valor do produto: ");
+                decimal valorProduto = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("Dinheiro na carteira: ");
+                decimal dinheiroCliente = decimal.Parse(Console.ReadLine());
+
+                if (valorProduto <= dinheiroCliente)
+                {
+                    troco = dinheiroCliente - valorProduto;
+                    Console.WriteLine($"Troco da compra: R${troco}");
+                    return troco;
+                }
+                else
+                {
+                    Console.WriteLine("Dinheiro insuficiente, não foi possível realizar a compra.");
+                    return 0;
+                }
+            }
+
+            //DevolverTroco - passando o valor da venda e o valor dado pelo cliente,
+            //mas que o método ja deposito o valor e saque o troco
+            public decimal DevolverTroco()
+            {
+                decimal troco = 0M;
+                Console.WriteLine($"Saldo do caixa: R$ {Saldo}");
+                Console.WriteLine("Próximo cliente.");
+                Console.WriteLine("Valor do produto: ");
+                decimal valorProduto = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("Valor recebido do cliente: ");
+                decimal pagamentoCliente = decimal.Parse(Console.ReadLine());
+
+                if (valorProduto <= pagamentoCliente)
+                {
+                    troco = pagamentoCliente - valorProduto;
+                    Console.WriteLine($"Troco da compra: R${troco}");
+                    decimal depositoCaixa = Saldo + valorProduto;
+                    decimal saldoAtual = Saldo - troco;
+                    Console.WriteLine($"Saldo do caixa: R${saldoAtual}");
+                    return saldoAtual;
+                }
+                else
+                {
+                    Console.WriteLine("Pagamento insuficiente, não foi possível realizar a compra.");
+                    return 0;
+                }
+            }
+        }
+}
